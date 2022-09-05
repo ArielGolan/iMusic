c830fa0e83d7a8473804d34c705ffd93f4d469c0
Ariel Golan <ariel.golan.ag@gmail.com>
2022-09-05 19:32:48 +0300

    Initial Commit

---
+++ iMusic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iMusic
+//
+//  Created by Ariel Golan on 05.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
