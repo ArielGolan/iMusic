bd141c6c9b263065ef466698b2c00e4cfd67e445
Ariel Golan <104303173+ArielGolan@users.noreply.github.com>
2022-09-20 15:43:20 +0300

    SceneDelegate Setup

---
+++ MainTabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabBarController.swift
+//  iMusic
+//
+//  Created by Ariel Golan on 20.09.2022.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .gray
+    }
+}
